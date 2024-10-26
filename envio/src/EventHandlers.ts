484cad5392de38894283e5a2a33ba3f7a5ace03c
Noah Sheldon <noahsheldon06@gmail.com>
2024-10-26 15:29:42 +0100

    index with usdc/usdt

--- envio/src/EventHandlers.ts
+++ envio/src/EventHandlers.ts
@@ -2,6 +2,24 @@
  * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
  */
 import {
+  FiatTokenV2_2,
+  FiatTokenV2_2_Approval,
+  FiatTokenV2_2_AuthorizationCanceled,
+  FiatTokenV2_2_AuthorizationUsed,
+  FiatTokenV2_2_Blacklisted,
+  FiatTokenV2_2_BlacklisterChanged,
+  FiatTokenV2_2_Burn,
+  FiatTokenV2_2_MasterMinterChanged,
+  FiatTokenV2_2_Mint,
+  FiatTokenV2_2_MinterConfigured,
+  FiatTokenV2_2_MinterRemoved,
+  FiatTokenV2_2_OwnershipTransferred,
+  FiatTokenV2_2_Pause,
+  FiatTokenV2_2_PauserChanged,
+  FiatTokenV2_2_RescuerChanged,
+  FiatTokenV2_2_Transfer,
+  FiatTokenV2_2_UnBlacklisted,
+  FiatTokenV2_2_Unpause,
   TetherToken,
   TetherToken_AddedBlackList,
   TetherToken_Approval,
@@ -15,6 +33,168 @@ import {
   TetherToken_Transfer,
   TetherToken_Unpause,
 } from "generated";
+
+FiatTokenV2_2.Approval.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_Approval = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    owner: event.params.owner,
+    spender: event.params.spender,
+    value: event.params.value,
+  };
+
+  context.FiatTokenV2_2_Approval.set(entity);
+});
+
+FiatTokenV2_2.AuthorizationCanceled.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_AuthorizationCanceled = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    authorizer: event.params.authorizer,
+    nonce: event.params.nonce,
+  };
+
+  context.FiatTokenV2_2_AuthorizationCanceled.set(entity);
+});
+
+FiatTokenV2_2.AuthorizationUsed.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_AuthorizationUsed = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    authorizer: event.params.authorizer,
+    nonce: event.params.nonce,
+  };
+
+  context.FiatTokenV2_2_AuthorizationUsed.set(entity);
+});
+
+FiatTokenV2_2.Blacklisted.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_Blacklisted = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    _account: event.params._account,
+  };
+
+  context.FiatTokenV2_2_Blacklisted.set(entity);
+});
+
+FiatTokenV2_2.BlacklisterChanged.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_BlacklisterChanged = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    newBlacklister: event.params.newBlacklister,
+  };
+
+  context.FiatTokenV2_2_BlacklisterChanged.set(entity);
+});
+
+FiatTokenV2_2.Burn.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_Burn = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    burner: event.params.burner,
+    amount: event.params.amount,
+  };
+
+  context.FiatTokenV2_2_Burn.set(entity);
+});
+
+FiatTokenV2_2.MasterMinterChanged.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_MasterMinterChanged = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    newMasterMinter: event.params.newMasterMinter,
+  };
+
+  context.FiatTokenV2_2_MasterMinterChanged.set(entity);
+});
+
+FiatTokenV2_2.Mint.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_Mint = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    minter: event.params.minter,
+    to: event.params.to,
+    amount: event.params.amount,
+  };
+
+  context.FiatTokenV2_2_Mint.set(entity);
+});
+
+FiatTokenV2_2.MinterConfigured.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_MinterConfigured = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    minter: event.params.minter,
+    minterAllowedAmount: event.params.minterAllowedAmount,
+  };
+
+  context.FiatTokenV2_2_MinterConfigured.set(entity);
+});
+
+FiatTokenV2_2.MinterRemoved.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_MinterRemoved = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    oldMinter: event.params.oldMinter,
+  };
+
+  context.FiatTokenV2_2_MinterRemoved.set(entity);
+});
+
+FiatTokenV2_2.OwnershipTransferred.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_OwnershipTransferred = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    previousOwner: event.params.previousOwner,
+    newOwner: event.params.newOwner,
+  };
+
+  context.FiatTokenV2_2_OwnershipTransferred.set(entity);
+});
+
+FiatTokenV2_2.Pause.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_Pause = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+  };
+
+  context.FiatTokenV2_2_Pause.set(entity);
+});
+
+FiatTokenV2_2.PauserChanged.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_PauserChanged = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    newAddress: event.params.newAddress,
+  };
+
+  context.FiatTokenV2_2_PauserChanged.set(entity);
+});
+
+FiatTokenV2_2.RescuerChanged.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_RescuerChanged = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    newRescuer: event.params.newRescuer,
+  };
+
+  context.FiatTokenV2_2_RescuerChanged.set(entity);
+});
+
+FiatTokenV2_2.Transfer.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_Transfer = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    from: event.params.from,
+    to: event.params.to,
+    value: event.params.value,
+  };
+
+  context.FiatTokenV2_2_Transfer.set(entity);
+});
+
+FiatTokenV2_2.UnBlacklisted.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_UnBlacklisted = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    _account: event.params._account,
+  };
+
+  context.FiatTokenV2_2_UnBlacklisted.set(entity);
+});
+
+FiatTokenV2_2.Unpause.handler(async ({ event, context }) => {
+  const entity: FiatTokenV2_2_Unpause = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+  };
+
+  context.FiatTokenV2_2_Unpause.set(entity);
+});
 
 TetherToken.AddedBlackList.handler(async ({ event, context }) => {
   const entity: TetherToken_AddedBlackList = {
--- envio/test/Test.ts
+++ envio/test/Test.ts
@@ -1,35 +1,37 @@
 import assert from "assert";
 import { 
   TestHelpers,
-  TetherToken_AddedBlackList
+  FiatTokenV2_2_Approval
 } from "generated";
-const { MockDb, TetherToken } = TestHelpers;
+const { MockDb, FiatTokenV2_2 } = TestHelpers;
 
-describe("TetherToken contract AddedBlackList event tests", () => {
+describe("FiatTokenV2_2 contract Approval event tests", () => {
   // Create mock db
   const mockDb = MockDb.createMockDb();
 
-  // Creating mock for TetherToken contract AddedBlackList event
-  const event = TetherToken.AddedBlackList.createMockEvent({/* It mocks event fields with default values. You can overwrite them if you need */});
+  // Creating mock for FiatTokenV2_2 contract Approval event
+  const event = FiatTokenV2_2.Approval.createMockEvent({/* It mocks event fields with default values. You can overwrite them if you need */});
 
-  it("TetherToken_AddedBlackList is created correctly", async () => {
+  it("FiatTokenV2_2_Approval is created correctly", async () => {
     // Processing the event
-    const mockDbUpdated = await TetherToken.AddedBlackList.processEvent({
+    const mockDbUpdated = await FiatTokenV2_2.Approval.processEvent({
       event,
       mockDb,
     });
 
     // Getting the actual entity from the mock database
-    let actualTetherTokenAddedBlackList = mockDbUpdated.entities.TetherToken_AddedBlackList.get(
+    let actualFiatTokenV2_2Approval = mockDbUpdated.entities.FiatTokenV2_2_Approval.get(
       `${event.chainId}_${event.block.number}_${event.logIndex}`
     );
 
     // Creating the expected entity
-    const expectedTetherTokenAddedBlackList: TetherToken_AddedBlackList = {
+    const expectedFiatTokenV2_2Approval: FiatTokenV2_2_Approval = {
       id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-      _user: event.params._user,
+      owner: event.params.owner,
+      spender: event.params.spender,
+      value: event.params.value,
     };
     // Asserting that the entity in the mock database is the same as the expected entity
-    assert.deepEqual(actualTetherTokenAddedBlackList, expectedTetherTokenAddedBlackList, "Actual TetherTokenAddedBlackList should be the same as the expectedTetherTokenAddedBlackList");
+    assert.deepEqual(actualFiatTokenV2_2Approval, expectedFiatTokenV2_2Approval, "Actual FiatTokenV2_2Approval should be the same as the expectedFiatTokenV2_2Approval");
   });
 });
